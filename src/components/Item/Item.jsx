fbf0b91908889c610a70601a55de7cb7fccfdb85
flortoledo85 <florenciaetoledo@gmail.com>
2026-05-10 19:33:11 -0300

    Relimpieza de imagenes. Carga productos.json

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -2,31 +2,47 @@ import { useState } from "react";
 import styles from "./Item.module.css"
 
 
-export function Item({ id, imagen, nombre, precio, stock }) {
+export function Item({ id, image, name, price, stock }) {
+
+    const [favourite, setFavourite] = useState(false);
+    
+    const toggleFavourite = () => {setFavourite(!favourite)};
+    
     const [counter, setCounter] = useState(0);
+    
     const increment = () => {
         if (counter < stock) {
             setCounter(counter + 1);
         };
     };
+    
     const decrement = () => {
         if (counter >= 1) {
             setCounter(counter - 1);
         };
     };
-    const CompraClick = () => { alert(`¡Agregaste ${counter} unidades de ${nombre} al carrito!`); };
+    const CompraClick = () => { };
     return (
         <div className={styles.producto}>
-            <img src={imagen} alt={nombre} className={styles.img}></img>
-            <h3 className={styles.name}>{nombre}</h3>
-            <p className={styles.precio}>Precio: ${precio}</p>
+            <img src={image} alt={name} className={styles.img}></img>
+            <h3 className={styles.name}>{name}</h3>
+            <p className={styles.precio}>Precio: ${price}</p>
+            <span onClick={toggleFavourite} className={styles.star} style={{'--color-star': favourite ? '#FFD700' :' #ccc'}}>{favourite ? '★' : '☆'}</span>
+            {/* El codigo abajo es como utilizar las propiedades de svg para lograr el mismo efecto sin css comentar
+            linea 30 y descomentar 33 para ver efecto*/}
+            {/* <span onClick={toggleFavourite}>{favourite ? <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="gold" stroke="gold" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-star-icon lucide-star"><path d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z"/></svg> : <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-star-icon lucide-star"><path d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z"/></svg>} </span> */}
             <p>Stock disponible: {stock}</p>
             <div className={styles.counter}>
                 <button onClick={decrement}>-</button>
-                    <p>{counter}</p>
+                <p>{counter}</p>
                 <button onClick={increment}>+</button>
             </div>
             <button onClick={CompraClick}>Agregar al Carrito</button>
         </div>
     );
 }
+
+
+// {favourite ? '': ''}
+
+{/*  */ }
--- src/components/ItemListCotainer/ItemListContainer.jsx
+++ src/components/ItemListCotainer/ItemListContainer.jsx
@@ -1,12 +1,21 @@
+import { useState, useEffect } from "react";
 import { ItemList } from "../ItemList/ItemList";
 import styles from "./ItemListContainer.module.css"
 
 export function ItemListCotainer({ Mensaje }) {
-    const productos = [
-        {id: '1234', nombre: 'Pink Love', precio: 120000, stock: 15, imagen: '/pictures/pink_love.jpg'},
-        {id: '1244', nombre: 'Ray Ban', precio: 450000, stock: 10, imagen: '/pictures/rayban_p.jpg'},
-        {id: '1254', nombre: 'Armonic', precio: 190000, stock: 18, imagen: '/pictures/cristal.jpg'},
-    ];
+    const [productos, setProducto] = useState([])
+
+    useEffect(() => {
+        fetch('/data/productos.json')
+        .then(res => res.json())
+        .then(data => setProducto(data))
+        .catch(error => console.error('Error', error))
+    }, []);
+    // const productos = [
+    //     {id: '1234', nombre: 'Pink Love', precio: 120000, stock: 15, imagen: '/pictures/pink_love.jpg'},
+    //     {id: '1244', nombre: 'Ray Ban', precio: 450000, stock: 10, imagen: '/pictures/rayban_p.jpg'},
+    //     {id: '1254', nombre: 'Armonic', precio: 190000, stock: 18, imagen: '/pictures/cristal.jpg'},
+    // ];
     return (
         <div>
             <h2 className={styles.subtitulo}> {Mensaje} </h2>
