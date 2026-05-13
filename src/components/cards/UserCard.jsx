81451d7ffbb8eb957217d85f81c06da67ffd9ee2
flortoledo85 <florenciaetoledo@gmail.com>
2026-05-13 20:44:13 -0300

    Adding cards for footer

--- src/components/cards/UserCard.jsx
+++ src/components/cards/UserCard.jsx
@@ -4,7 +4,7 @@ import styles from "./UserCard.module.css";
 export function UserCard({ name, profile, image }){
     return(
         <div className={styles.card}>
-            <img scr={image} alt={name} className={styles.image}/>
+            <img src={image} alt={name} className={styles.imagen}/>
             <h3>{name}</h3>
             <p className={styles.description}>{profile}</p>
         </div>
--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,4 +1,4 @@
-import './Footer.css';
+import styles from './Footer.module.css';
 import React from 'react';
 import { UserCard } from '../cards/UserCard';
 
@@ -10,8 +10,26 @@ function Footer(){
         { name: "Laura Martínez", profile: "Costumer Service", image: "/pictures/laura.jpg" },
     ];
     return(
-        <footer className="footer">
-                <p>&copy; 2026 - Optic Choices</p>
+        <footer className={styles.footer}>
+            <div className= {styles.contact}>
+                <h3>Optic Choices</h3>
+                <p>La Plata, Buenos Aires</p>
+                <p>contacto@opticchoices.com</p>
+            </div>
+            <div className={styles.team}>
+                <h4>Our Team</h4>
+                <div className={styles.cards}>
+                    {team.map((profesional, index) => (
+                        <UserCard
+                            key={index}
+                            image={profesional.image}
+                            name={profesional.name}
+                            profile={profesional.profile}
+                            />
+                        ))}
+                </div>
+            </div>
+                <p className={styles.copy}>&copy; 2026 - Optic Choices</p>
         </footer>
     );
 }
