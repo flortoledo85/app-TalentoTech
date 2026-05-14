e2a65160986a68afa23934726bd3fcd2deb9383b
flortoledo85 <florenciaetoledo@gmail.com>
2026-05-14 08:38:27 -0300

    details

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -5,8 +5,8 @@ import { UserCard } from '../cards/UserCard';
 
 function Footer(){
     const team = [
-        { name: "Ana García", profile: "Desing and UX", image: "/pictures/ana.jpg" },
-        { name: "Matías López", profile: "Frontend Development", image: "/pictures/matias.jpg" },
+        { name: "Ana García", profile: "Optician", image: "/pictures/ana.jpg" },
+        { name: "Matías López", profile: "Optician-Optometrist", image: "/pictures/matias.jpg" },
         { name: "Laura Martínez", profile: "Costumer Service", image: "/pictures/laura.jpg" },
     ];
     return(
