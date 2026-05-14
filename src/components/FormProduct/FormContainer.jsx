8fa0181899219de6ef10c19e16845bc372051e2d
flortoledo85 <florenciaetoledo@gmail.com>
2026-05-13 21:08:24 -0300

    error handler in form

--- src/components/FormProduct/FormContainer.jsx
+++ src/components/FormProduct/FormContainer.jsx
@@ -25,24 +25,38 @@ export function FormContainer() {
 
     const handleSend = async (evento) => { 
         evento.preventDefault();
+        if (!imageFile) {
+            alert("You should choose a picture for the product.");
+            return;
+        }
         const apiKey = '9185cdcb00872d80ffeeab4601937375';
         const formData = new FormData();
         formData.append("image", imageFile);
+        try {
+            console.log("Charging image to Imgbb...")
+            const answerImgbb = await fetch(`https://api.imgbb.com/1/upload?key=${apiKey}`,
+                {
+                    method: 'POST',
+                    body: formData,
+                });
 
-        const answerImgbb = await fetch(`https://api.imgbb.com/1/upload?key=${apiKey}`,
-            {
-                method: 'POST',
-                body: formData,
-            });
+            const imgbbData = await answerImgbb.json();
+            
+            if (imgbbData.success) {
+                console.log("Picture uploaded correctly. URL: ", imgbbData.data.url);
 
-        const imgbbData = await answerImgbb.json();
-        const urlImage = imgbbData.data.url;
+            const urlImage = imgbbData.data.url;
 
-        const fullProduct = { ...dataForm, urlImage: urlImage};
-        console.log('The product is ready for send: ', fullProduct);
+            const fullProduct = { ...dataForm, urlImage: urlImage};
+            console.log('The product is ready for send: ', fullProduct);
+            } else {
+                throw new Error("Failed upload to Imgbb.");
+            }
 
+        } catch (error) {
+            console.error("Error sending data: ", error);
+        }
     };
-
     return (
         <div className={styles.containerForm}>
             <FormProduct
