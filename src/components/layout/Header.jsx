36d87c377c6aa6a4746a3d0f0fe629b23df2612b
flortoledo85 <florenciaetoledo@gmail.com>
2026-07-05 17:25:39 -0300

    added responsiveness to management section

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import styles from "./Header.module.css";
 import { Link } from "react-router-dom";
 import { useCart } from "../../context/CartContext";
-import { ShoppingCart } from "lucide-react";
+import { ShoppingCart, User, LogIn } from "lucide-react";
 import { useAuth } from "../../context/AuthContext";
 import { useSearch } from "../../context/SearchContext";
 import { useNavigate } from "react-router-dom";
@@ -25,19 +25,11 @@ function Header() {
             <div className={styles.topBar}>
                 <h1 className={styles.logo}>Optic Choices</h1>
                 <div className={styles.userActions}>
-                    {user ? (
-                        <>
-                            {user.rol === 'admin' && (
-                                <Link to="/management" className={styles.managementBtn}>Management</Link>
-                            )}
-                            <span>Hi, {user.email}</span>
-                            <button onClick={logout}>Close session</button>
-                        </>
-                    ) : (
-                        <Link to="/login">Login</Link>
-                    )}
+                    <Link to={user ? "/" : "/login"} className={styles.userIcon}>
+                        {user ? <User /> : <LogIn/>}
+                    </Link>
                     <Link to="/cart"><ShoppingCart /> {totalItems > 0 && <span>{totalItems}</span>} </Link>
-                    <button className={`${styles.hamburger} ${menuOpen ? styles.hamburger : ''}`}
+                    <button className={`${styles.hamburger} ${menuOpen ? styles.hamburgerOpen : ''}`}
                         onClick={() => setMenuOpen(!menuOpen)}>
                         <span></span>
                         <span></span>
@@ -49,6 +41,15 @@ function Header() {
                 <Link to="/" onClick={() => setMenuOpen(false)}>Home</Link>
                 <Link to="/products" onClick={() => setMenuOpen(false)}>Products</Link>
                 <Link to="/measurements" onClick={() => setMenuOpen(false)}>Measurements</Link>
+                {user && (
+                    <>
+                        {user.rol === 'admin' && (
+                            <Link to="/management" onClick={() => setMenuOpen(false)}>Management</Link>
+                        )}
+                        <span>Hi, {user.email}</span>
+                        <button className={styles.closeBtn} onClick={logout}>Close session</button>
+                    </>
+                )}
                 <input
                     type="text"
                     className={styles.searchInput}
